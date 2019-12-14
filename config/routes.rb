8357c2c839cb7a50cceb78e15371a01ddf431e4d
Paula <paula.beltran.pb@gmail.com>
2019-12-14 11:40:19 +0100

    creation of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # lire tous les tasks
+  get 'tasks', to: 'tasks#index'
+
+  # lire un task
+  get 'tasks/:id', to: 'tasks#show'
+
+  # creer un task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # updater un restaurant
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # supprimer un task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
